65271a8180a75b921a20f423432e7c9b302a05b4
Michael Hermansen <michael.p.hermansen@gmail.com>
2021-07-14 23:07:10 +0200

    seo og favicon

--- pages/index.js
+++ pages/index.js
@@ -26,6 +26,29 @@ export default function Home() {
 				<meta name='description' content='Lag og del forkortede URL-er.' />
 				<meta name='author' content='Michael Hermansen' />
 
+				{/* favicon-config */}
+				<link
+					rel='apple-touch-icon'
+					sizes='180x180'
+					href='/apple-touch-icon.png'
+				/>
+				<link
+					rel='icon'
+					type='image/png'
+					sizes='32x32'
+					href='/favicon-32x32.png'
+				/>
+				<link
+					rel='icon'
+					type='image/png'
+					sizes='16x16'
+					href='/favicon-16x16.png'
+				/>
+				<link rel='manifest' href='/site.webmanifest' />
+				<link rel='mask-icon' href='/safari-pinned-tab.svg' color='#8f8fff' />
+				<meta name='msapplication-TileColor' content='#8f8fff' />
+				<meta name='theme-color' content='#8f8fff'></meta>
+
 				{/* polyfill for 'scrollBehavior: smooth' */}
 				<script
 					defer
