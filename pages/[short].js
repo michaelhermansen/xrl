e79c7fbc53054837d31794ce1fd3565f2987a655
Michael Hermansen <michael.p.hermansen@gmail.com>
2021-08-27 03:20:44 +0200

    mye bedre løsning som ikke gir feilmelding.....

--- pages/[short].js
+++ pages/[short].js
@@ -1,36 +1,33 @@
 import mongoConnect from '@utils/mongoConnect'
 import ShortLink from '@models/ShortLink'
-import { useEffect } from 'react'
 
 export async function getServerSideProps(context) {
 	await mongoConnect()
 	await ShortLink.init()
-	let url
 
 	try {
 		const link = await ShortLink.findOne({ short: context.params.short })
 		if (link) {
 			link.clicks++
-			link.expiresAt = Date.now() + 1000 * 60 * 60 * 24 * 30 // 30 dager frem i tid
+			link.expiresAt = Date.now() + 1000 * 60 * 60 * 24 * 180 // 180 dager frem i tid
 			link.save()
-			url = link.original
+
+			return {
+				redirect: {
+					permanent: false,
+					destination: link.original,
+				},
+			}
 		}
 	} catch (error) {
 		console.error(error)
 	}
 
 	return {
-		props: {
-			url,
-		},
-		notFound: url ? false : true,
+		notFound: true,
 	}
 }
 
-export default function Redirect({ url }) {
-	useEffect(() => {
-		window.location.replace(url)
-	})
-
+export default function Null() {
 	return null
 }
