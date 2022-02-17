1529016f88936b8e1c2f19e4de3a3eba0973c704
Michael Hermansen <michael.p.hermansen@gmail.com>
2022-02-18 00:43:48 +0100

    Oppdatert URL-regex

--- utils/validateURL.js
+++ utils/validateURL.js
@@ -1,17 +1,17 @@
 const urlRegex =
-	/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g
+  /[-a-zA-Z0-9@:%.,_\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.,~#?&//=]*)/g;
 
-const xrlRegex = /.*xrl\.no\/..*|.*xrl\.vercel\.app\/..*/
+const xrlRegex = /.*xrl\.no\/..*|.*xrl\.vercel\.app\/..*/;
 
-const validateURL = url => {
-	const match = url.match(urlRegex)
-	const isXrl = xrlRegex.test(url)
+const validateURL = (url) => {
+  const match = url.match(urlRegex);
+  const isXrl = xrlRegex.test(url);
 
-	return {
-		valid: match ? true : false,
-		cleanURL: match ? `https://${match[0].trim()}` : null,
-		isXrl,
-	}
-}
+  return {
+    valid: match ? true : false,
+    cleanURL: match ? `https://${match[0].trim()}` : null,
+    isXrl,
+  };
+};
 
-export default validateURL
+export default validateURL;
