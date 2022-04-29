90cf2d05a0610ee6b19a3ea1a0ecbb03765eb433
Michael Hermansen <michael.p.hermansen@gmail.com>
2022-04-29 18:05:17 +0200

    Replaced "shortid" with "nanoid"

--- models/ShortLink.js
+++ models/ShortLink.js
@@ -1,44 +1,49 @@
-import mongoose from 'mongoose'
-import shortId from 'shortid'
-import validateURL from '@utils/validateURL'
-import testAlias from '@utils/testAlias'
+import mongoose from "mongoose";
+import validateURL from "@utils/validateURL";
+import testAlias from "@utils/testAlias";
+import { customAlphabet } from "nanoid";
+
+const nanoid = customAlphabet(
+  "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  6
+);
 
 const shortLinkSchema = new mongoose.Schema({
-	original: {
-		type: String,
-		required: [true, 'Ugyldig URL'],
-	},
-	short: {
-		type: String,
-		unique: [true, 'Alias er allerede i bruk'],
-		required: true,
-		maxLength: 20,
-		default: shortId.generate,
-	},
-	clicks: {
-		type: Number,
-		required: true,
-		default: 0,
-	},
-	userID: {
-		type: String,
-		required: false,
-	},
-	expiresAt: {
-		type: Date,
-		required: true,
-		default: Date.now() + 1000 * 60 * 60 * 24 * 30, // 30 dager frem i tid
-	},
-})
+  original: {
+    type: String,
+    required: [true, "Ugyldig URL"],
+  },
+  short: {
+    type: String,
+    unique: [true, "Alias er allerede i bruk"],
+    required: true,
+    maxLength: 20,
+    default: nanoid,
+  },
+  clicks: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  userID: {
+    type: String,
+    required: false,
+  },
+  expiresAt: {
+    type: Date,
+    required: true,
+    default: Date.now() + 1000 * 60 * 60 * 24 * 30, // 30 dager frem i tid
+  },
+});
 
-shortLinkSchema.path('original').validate(url => {
-	if (!validateURL(url).valid) return false
-	if (validateURL(url).isXrl) return false
-}, 'Ugyldig URL')
+shortLinkSchema.path("original").validate((url) => {
+  if (!validateURL(url).valid) return false;
+  if (validateURL(url).isXrl) return false;
+}, "Ugyldig URL");
 
-shortLinkSchema.path('short').validate(short => {
-	return testAlias(short)
-}, 'Ugyldig alias')
+shortLinkSchema.path("short").validate((short) => {
+  return testAlias(short);
+}, "Ugyldig alias");
 
 export default mongoose.models.ShortLink ||
-	mongoose.model('ShortLink', shortLinkSchema)
+  mongoose.model("ShortLink", shortLinkSchema);
